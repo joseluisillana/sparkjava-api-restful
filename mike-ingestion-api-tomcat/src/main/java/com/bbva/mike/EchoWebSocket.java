f44606b7e8e72576814ac107a7b7f814f0b3e2d0
joseluis.illana@beeva.com <joseluis.illana@beeva.com>
2015-12-10 17:45:16 +0100

    Subida inicial para tomcat con intellij y limpieza del preparado para jetty con mínima configuración

---
+++ mike-ingestion-api-tomcat/src/main/java/com/bbva/mike/EchoWebSocket.java
@@ -0,0 +1,35 @@
+package com.bbva.mike;
+
+import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * Created by joseluisillana on 10/12/15.
+ */
+@WebSocket
+public class EchoWebSocket {
+
+    // Store sessions if you want to, for example, broadcast a message to all users
+    private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
+
+    @OnWebSocketConnect
+    public void connected(Session session) {
+        sessions.add(session);
+    }
+
+    @OnWebSocketClose
+    public void closed(Session session, int statusCode, String reason) {
+        sessions.remove(session);
+    }
+
+    @OnWebSocketMessage
+    public void message(Session session, String message) throws IOException {
+        System.out.println("Got: " + message);   // Print message
+        session.getRemote().sendString(message); // and send it back
+    }
+
+}
+
